7a6076eb52d94c4ef69b9cf7ccec6459d25b26c2
Ilfat <ilfat.gubaidullin@gmail.com>
2018-04-12 16:00:00 +0500

    ++++

---
+++ ПР_11.pas
@@ -0,0 +1,19 @@
+﻿var i,j,n,m,p,s,x: integer;
+begin
+  Write('x: ');
+  readln(x);
+  p:= 1;
+  for n:= 1 to 3 do
+  begin
+    for m:= 0 to 5 do
+    begin
+        for j:= 0 to m do
+        begin
+          s:=(n + j + x);
+          p:=p*s;
+        end;
+      writeln('y(',n,',',m+1,j,') = ', p);
+      p:=1;
+    end;
+  end;
+end.
---
+++ ПР_12.pas
@@ -0,0 +1,19 @@
+﻿var i,j,x,y,g: integer;
+p,m,s:real;
+begin
+  Write('x: ');
+  readln(x);
+  p:= 1;
+  s:= 1;
+  for i:= 1 to 3 do
+  begin
+    for j:= 0 to 2 do
+    begin
+      m:=(x + ln(i + 1 + j));
+      p:=p*m;
+    end;
+    s:= s*p;
+    p:= 1;
+  end;
+write(s:0:2)
+end.
